eb7621c1bd190da2e121d629e2963ad68615d2b4
dwea <62627481+dwea@users.noreply.github.com>
2025-07-23 12:31:06 -0700

    Update bodyFilter.js

--- new/bodyFilter.js
+++ new/bodyFilter.js
@@ -1,15 +1,14 @@
-// Load your SVG into the #body div
 fetch('./assets/human-body.svg')
   .then(res => res.text())
   .then(svg => {
     document.getElementById('body').innerHTML = svg;
 
-    // Make regions clickable
     document.querySelectorAll('#body svg [id]').forEach(region => {
       region.style.cursor = 'pointer';
       region.addEventListener('click', () => {
         const event = new CustomEvent('bodyRegionSelected', { detail: region.id });
         window.dispatchEvent(event);
+        console.log(`Clicked region: ${region.id}`);
       });
     });
   });
